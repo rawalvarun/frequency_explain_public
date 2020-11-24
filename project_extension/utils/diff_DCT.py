18de0803b0b0b94faf9681c13271fe80217c4028
rawalvarun <rawalvarun74@gmail.com>
2020-11-24 18:55:47 -0500

    fix DCT diff script

--- project_extension/utils/diff_DCT.py
+++ project_extension/utils/diff_DCT.py
@@ -1,6 +1,12 @@
 
 '''
-python utils/diff_DCT.py --first ./generic_pipeline/alexnet_finetune_BFGS/correct_DCT_targeted.png  --second ./generic_pipeline/densenet_BFGS/correct_DCT_targeted.png 
+python diff_DCT.py --first ../generic_pipeline/alexnet_finetune_BFGS/correct_DCT_targeted.png  --second ../generic_pipeline/densenet_BFGS/correct_DCT_targeted.png 
+
+adding --all or not can lead to more or few diff points
+
+
+python diff_DCT.py --first ../generic_pipeline/small_batch/densenet_PGD/logscale_correct_DCT_targeted.png --second ../generic_pipeline/small_batch/squeezenet_PGD/logscale_correct_DCT_targeted.png
+
 '''
 
 # import the necessary packages
@@ -8,13 +14,23 @@ from skimage.measure import compare_ssim
 import argparse
 import imutils
 import cv2
+import os
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-f", "--first", required=True,
 	help="first input image")
 ap.add_argument("-s", "--second", required=True,
 	help="second")
+ap.add_argument("-a", "--all", required=False,  action='store_true',
+	help="show all differences or only the highest ones ")
 args = vars(ap.parse_args())
+
+if not os.path.exists(args["first"]):
+	raise Exception("First file doesn't exist")
+
+if not os.path.exists(args["second"]):
+	raise Exception("Second file doesn't exist")
 
 # load the two input images
 imageA = cv2.imread(args["first"])
@@ -78,8 +94,7 @@ after_gray = cv2.cvtColor(after, cv2.COLOR_BGR2GRAY)
 
 # Compute SSIM between two images
 (score, diff) = compare_ssim(before_gray, after_gray, full=True)
-#diff = before_gray - after_gray
-
+diff = 1 - (cv2.absdiff(before_gray, after_gray) / 255)
 
 before_gray = before_gray.astype("int32")
 after_gray = after_gray.astype("int32")
@@ -93,6 +108,11 @@ print(np.unique(_a_min_b_))
 _subtract_ = after_gray - before_gray
 _b_min_a_ = (_subtract_ <= 0.0).astype("uint8")
 
+print("!!!")
+print(f"{np.count_nonzero(_a_min_b_)} non zeros")
+print(f"{np.count_nonzero(_b_min_a_)} non zeros")
+print("!!!")
+
 print(np.unique(_subtract_))
 print(np.unique(_b_min_a_))
 
@@ -100,12 +120,51 @@ print(np.unique(_b_min_a_))
 print(f"{np.count_nonzero(diff)} non zeros")
 print(diff)
 
-diff = diff * _b_min_a_
-#cv2.bitwise_and(diff, diff, _b_min_a_)
+#cv2.imwrite('diff_Only.png',255*diff)
+
+
+
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+'''
+Choose either 1st two or last two depending upon number of points needed 
+'''
+
+if args['all']:
+	diff1 = _a_min_b_
+	diff2 = _b_min_a_
+else:
+	diff1 = 1 - ((1 - diff) * _a_min_b_)
+	diff2 = 1 - ((1 - diff) * _b_min_a_)
+
+
+################################################################################
+################################################################################
+################################################################################
+################################################################################
+
+
+
+
+
+
+
+
+
+
+
+#cv2.bitwise_and(diff, diff, _a_min_b_)
 
 print("\n\n")
 print(diff)
 print(f"{np.count_nonzero(diff)} non zeros")
+
+#cv2.imwrite('diff___b_min_a_.png',255*diff)
+
+
+
 
 
 
@@ -118,7 +177,8 @@ print("Image similarity", score)
 # so we must convert the array to 8-bit unsigned integers in the range
 # [0,255] before we can use it with OpenCV
 
-diff = (diff * 255).astype("uint8")
+diff1 = (diff1 * 255).astype("uint8")
+diff2 = (diff2 * 255).astype("uint8")
 
 
 # Threshold the difference image, followed by finding contours to
@@ -129,26 +189,42 @@ thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
 contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 '''
 
-thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+thresh1 = cv2.threshold(diff1, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+contours1 = cv2.findContours(thresh1.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-contours = contours[0] if len(contours) == 2 else contours[1]
+thresh2 = cv2.threshold(diff2, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+contours2 = cv2.findContours(thresh2.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+contours1 = contours1[0] if len(contours1) == 2 else contours1[1]
+contours2 = contours2[0] if len(contours2) == 2 else contours2[1]
 
 mask = np.zeros(before.shape, dtype='uint8')
 filled_after = after.copy()
 
-for c in contours:
+for c in contours1:
     area = cv2.contourArea(c)
-    if area > 40:
+    if area > 5:
         x,y,w,h = cv2.boundingRect(c)
         cv2.rectangle(before, (x, y), (x + w, y + h), (12, 24, 251), 2)
-        cv2.rectangle(after, (x, y), (x + w, y + h), (36,255,12), 2)
+        cv2.rectangle(after, (x, y), (x + w, y + h), (12, 24, 251), 2)
         cv2.drawContours(mask, [c], 0, (0,255,0), -1)
         cv2.drawContours(filled_after, [c], 0, (0,255,0), -1)
+
+for c in contours2:
+    area = cv2.contourArea(c)
+    if area > 5:
+        x,y,w,h = cv2.boundingRect(c)
+        cv2.rectangle(before, (x, y), (x + w, y + h), (36,255,12), 2)
+        cv2.rectangle(after, (x, y), (x + w, y + h), (36,255,12), 2)
+        cv2.drawContours(mask, [c], 0, (255,0,255), -1)
+        cv2.drawContours(filled_after, [c], 0, (255,0,255), -1)
+
 
 cv2.imwrite('before.png', before)
 cv2.imwrite('after.png', after)
 cv2.imwrite('diff.png',diff)
 cv2.imwrite('mask.png',mask)
 cv2.imwrite('filled after.png',filled_after)
+
+
 cv2.waitKey(0)
